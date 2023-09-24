f9ecac33ab3843a69aa9fdd1f1833a25b15d2c33
Willem April <141494375+Buddynice34@users.noreply.github.com>
2023-09-24 09:29:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// Function to fetch and populate menu items from menu.json
+async function fetchAndPopulateMenuItems() {
+    try {
+        const response = await fetch("pizzas.json"); // Fetch the JSON file
+        const pizzaMenu = await response.json(); // Parse JSON data
+
+        const menuItemsContainer = document.getElementById("menu-items");
+
+        pizzaMenu.forEach((pizza, index) => {
+            const menuItem = document.createElement("div");
+            menuItem.classList.add("menu-item");
+            const menuItemHTML = `
+            <div class="col">
+                <div class="card mb-4">
+                    <img src="${pizza.image}" class="card-img-top" alt="${pizza.name}">
+                    <div class="card-body">
+                        <h5 class="card-title">${pizza.name}</h5>
+                        <p class="card-text">${pizza.description}</p>
+                        <button class="btn btn-primary">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        `;
+            const image = document.createElement("img");
+            image.src = pizza.image;
+            image.alt = pizza.name;
+
+            const name = document.createElement("h3");
+            name.textContent = pizza.name;
+
+            const description = document.createElement("p");
+            description.textContent = pizza.description;
+
+            menuItem.appendChild(image);
+            menuItem.appendChild(name);
+            menuItem.appendChild(description);
+
+            menuItemsContainer.innerHTML += menuItemHTML;
+        });
+    } catch (error) {
+        console.error("Error fetching and parsing JSON:", error);
+    }
+}
+
+// Use DOMContentLoaded event to ensure the DOM is fully loaded before running the script
+document.addEventListener("DOMContentLoaded", fetchAndPopulateMenuItems);
